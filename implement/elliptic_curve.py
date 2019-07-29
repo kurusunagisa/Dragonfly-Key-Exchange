46ca50d4c3a1e566a4bb8a5749dee1713f03aca1
yawarakakuma's copy <43271603+escapisms@users.noreply.github.com>
2019-07-29 15:45:24 +0900

    commit

--- implement/elliptic_curve.py
+++ implement/elliptic_curve.py
@@ -2,23 +2,59 @@ import numpy as np
 import math as mt
 import random as rd
 import secrets as sc
+import hashlib as hl
 
 a = 10000000000000
-b = 10000
+b = 10000000000
 p = 65537
+n = sc.randbelow(10000)
+l = sc.randbelow(10000)
+password :bytes = 1234
+k :bytes = 256
+class ellipticCurve:
+    def __init__(self):
+        self.a = a
+        self.b = b
+        self.p = p
 
-def ellipticCurve(x):
-    y = mt.sqrt(x ** 3 + a * x + b) % p
-    return y
+    def yCalc(self,x):
+        return mt.sqrt(x ** 3 + self.a * x + self.b)
 
-def primeNumber(n):
-    return sc.randbelow(n)
+class ellipticCurvePoint:
+    def __init__(self,x,y):
+        self.x :int= x
+        self.y :int= y
+
+
+    def add(self, o, r,val):
+        ram: float
+        if o.x == r.x:
+            ram = ((3 * o.y)**2 + val.a)/(2 * r.y)
+        else:
+            ram = ((r.y - o.y) / (r.x - o.x))
+        self.x = ram ** 2 - (o.x + r.x)
+        self.y :int= (-ram * (self.x - o.x) - o.y) % p
+
+class Hash:
+
+    def __init__(self, a, b, p):
+        self.a = a
+
+    def makeHash(a,b,password,counter):
+        H = hl.shake_256()
+        old = a + b + password + counter
+        H.update(old)
+        return H.hexdigest(256)
 
 
 if __name__ == "__main__":
-    n = 56636
-    prime = primeNumber(n)
-    print(prime)
-
-    Y = ellipticCurve(prime)
-    print(round(Y))
+    val = ellipticCurve()
+    Y = ellipticCurve.yCalc(val,n)
+    d = ellipticCurvePoint(n, Y)
+    Y = ellipticCurve.yCalc(val,l)
+    e = ellipticCurvePoint(l,Y)
+    print(d.y % p)
+    print(e.y % p)
+    f = ellipticCurvePoint(0,0)
+    ellipticCurvePoint.add(f, d, e, val)
+    print(f.y)
